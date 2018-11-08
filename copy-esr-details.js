690188385ccb22470424a5f2d0c260c360c0c7ea
Tyler Muth <tylerdmuth-git@yahoo.com>
2018-11-08 14:20:15 -0500

    Create copy-esr-details.js

---
+++ copy-esr-details.js
@@ -0,0 +1,51 @@
+// ==UserScript==
+// @name         Copy ESR Details to Clipboard
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       Tyler Muth - tmuth@splunk.com
+// @match        https://splunk.my.salesforce.com/00O0*
+// @grant        GM_setClipboard
+// @updateURL    https://gist.githubusercontent.com/tmuth/3724b09b71d4621c6adf7e053c3aa04a/raw/985a707e65350305410975b58445080d8647c0a0/sfdc-esr-copy.js
+// @downloadURL  https://gist.githubusercontent.com/tmuth/3724b09b71d4621c6adf7e053c3aa04a/raw/985a707e65350305410975b58445080d8647c0a0/sfdc-esr-copy.js
+// @require http://code.jquery.com/jquery-latest.js
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    function getBaseUrl() {
+        var re = new RegExp(/^.*\//);
+        return re.exec(window.location.href);
+    }
+
+    function getCellByHeaderName(el,search){
+        var indexId = $(el).parents('table.tabularReportTable').find('th:contains('+search+')').index();
+        indexId = indexId + 1;
+        return $(el).parents('tr').find('td:nth-child('+indexId+')');
+    }
+    function copyEsrTextToClipboard(el){
+        var url = getBaseUrl();
+        var esrLink = getCellByHeaderName(el,'Support Request Name').find('a').attr('href') ;
+        esrLink = url+esrLink;
+
+        var oppName = getCellByHeaderName(el,'Opportunity Name').find('a').prop('innerHTML');
+
+        var amount = getCellByHeaderName(el,'Amount').html();
+        amount = amount.replace('USD ','$');
+        var regex = /\.\d\d$/i;
+        amount = amount.replace(regex,'');
+
+        var outText = "ESR ~ " + oppName + " ~ " + amount + " ~ " +esrLink;
+        GM_setClipboard (outText);
+
+    }
+
+    if($('h1:contains(ESR)').length > 0){
+        $("a:contains(SSSR-)").parent("td").append('<span class="copyESRtoClipboard"> [copy] </span>');
+
+        $(".copyESRtoClipboard").click(function(){
+           copyEsrTextToClipboard(this);
+        })
+    }
+})();
